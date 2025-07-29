81d47bf14a1fa77d056131e1c5f6c63e21dc62ea
seoyeon <dlatjdus0303@naver.com>
2025-07-29 14:56:42 +0900

    test

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,4 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping("/hi")
+    public String test(){
+        return "내가 만든 서버";
+    }
 }
